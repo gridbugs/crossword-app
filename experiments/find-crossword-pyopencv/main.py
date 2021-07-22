b3b2fb6d95105de09c93fe0a7f1b09cd43e53fc0
Stephen Sherratt <stephen@sherra.tt>
2021-07-22 17:56:55 +1000

    Experiment with contour hierarchy

--- experiments/find-crossword-pyopencv/main.py
+++ experiments/find-crossword-pyopencv/main.py
@@ -1,6 +1,11 @@
 import argparse
 import cv2 as cv
 
+def contour_is_quad(contour):
+    APPROX_POLYGON_EPS_RATIO_OF_PERIMETER = 0.02
+    perimeter = cv.arcLength(contour, True)
+    approx_polygon = cv.approxPolyDP(contour, APPROX_POLYGON_EPS_RATIO_OF_PERIMETER * perimeter, True)
+    return len(approx_polygon) == 4
 
 def main(args):
     image = cv.imread(args.image)
@@ -8,10 +13,19 @@ def main(args):
     # convert to greyscale
     grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-    # apply a blur filter
-    blurred = cv.GaussianBlur(grey, (7, 7), 3)
+    inverted = cv.bitwise_not(grey)
 
-    cv.imshow("debug", blurred)
+    (contours, _hierarchy) = cv.findContours(inverted, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    (boundary_idx, boundary) = max(enumerate(contours), key = lambda c: cv.contourArea(c[1]))
+
+    for (contour_index, contour) in enumerate(contours):
+        if contour_is_quad(contour):
+            cv.drawContours(image, [contour], 0, (0, 255, 0), 2)
+
+    cv.drawContours(image, [boundary], 0, (0, 0, 255), 2)
+
+    cv.imshow("debug", image)
     cv.waitKey(0)
 
 
